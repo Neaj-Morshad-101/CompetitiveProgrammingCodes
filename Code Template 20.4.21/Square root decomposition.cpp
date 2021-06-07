5ab8a317ee65250625edb3e954d12ff44adb5bf2
Neaj Morshad <45729617+Neaj-Morshad-101@users.noreply.github.com>
2021-06-07 17:30:14 +0600

    Update Square root decomposition.cpp

--- Code Template 20.4.21/Square root decomposition.cpp
+++ Code Template 20.4.21/Square root decomposition.cpp
@@ -3,57 +3,67 @@ using namespace std;
 int b[100]; // array for storing result for each block
 
 ///O(n)
-int precalculate(int a[],int n){
-	int segment_size=sqrt(n);
-	int current_segment=-1; //Block Index
-	for(int i=0;i<n;i++){
-		if(i%segment_size==0){
-			++current_segment; //
-		}
-		b[current_segment]+=a[i];
-	}
-        return segment_size;
+int precalculate(int a[],int n)
+{
+    int segment_size=sqrt(n);
+    int current_segment=-1; //Block Index
+    for(int i=0; i<n; i++)
+    {
+        if(i%segment_size==0)
+        {
+            ++current_segment; //
+        }
+        b[current_segment]+=a[i];
+    }
+    return segment_size;
 }
 
 ///O(sqrt(n))
-int query(int l,int r,int a[],int n){
-	int segment_size=sqrt(n);
-	int sum=0;
+int query(int l,int r,int a[],int n)
+{
+    int segment_size=sqrt(n);
+    int sum=0;
 
 
     //First Part partially overlapped
-	while(l<r&&l%segment_size!=0){
-		sum+=a[l];
-		++l;
-	}
+    while(l<r&&l%segment_size!=0)
+    {
+        sum+=a[l];
+        ++l;
+    }
     // Second Part Fully overlapped
-	while(l+segment_size<=r){
-		sum+=b[l/segment_size];
-		l+=segment_size;
-	}
+    while(l+segment_size<=r)
+    {
+        sum+=b[l/segment_size];
+        l+=segment_size;
+    }
     //Third part partially overlapped
-	while(l<=r){
-		sum+=a[l];
-		l++;
-	}
-	return sum;
+    while(l<=r)
+    {
+        sum+=a[l];
+        l++;
+    }
+    return sum;
 }
 
 ///O(1)
-void update(int a[], int n, int i, int val) {
+void update(int a[], int n, int i, int val)
+{
     int segment_size=sqrt(n);
     int segment_no = i / segment_size;
     b[segment_no] -= a[i];
     b[segment_no] += val;
     a[i] = val;
 }
-int main(){
-	int n;
-	cin>>n;
-	int a[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-	precalculate(a,n);
-	cout<<query(1,10,a,n)<<endl;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    precalculate(a,n);
+    cout<<query(0,4,a,n)<<endl;
 }
